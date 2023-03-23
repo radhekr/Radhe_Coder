16d9079236892b88da201f815298dcd452e1f481
radhekr <127528964+radhekr@users.noreply.github.com>
2023-03-23 10:38:26 +0530

    hiiiiiiiii
    hiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ New_Project/Project_2/registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regitstration </title>
+</head>
+<body>
+    <h2>Registration</h2>
+    <form action="" class="" method="POST" autocomplete="off">
+    <label for="name" >Name : </label>
+    <input type="text" name="name" id="name" required value=""><br>
+    <label for="username" >User Name : </label>
+    <input type="text" name="username" id="username" required value=""><br>
+    <label for="email" >Email-Id : </label>
+    <input type="email" name="email" id="email" required value=""><br>
+    <label for="password" >Password : </label>
+    <input type="password" name="password" id="password" required value=""><br>
+</form>
+</body>
+</html>
